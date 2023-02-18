435ec97dff974249be11a54f90c8eeb820640b8b
Sujin <tnwlsk0190@gmail.com>
2023-02-18 22:33:58 +0900

    🚧 메인페이지 작업중

---
+++ src/PageStyles.js
@@ -0,0 +1,74 @@
+import styled from 'styled-components';
+
+export const BackTitle = styled.div`
+  font-size: 40px;
+  color: #f4e7c5;
+  font-weight: bold;
+  margin: 670px 0 10px 100px;
+  position: fixed;
+`;
+
+export const PageWrapper = styled.div`
+  background-color: #fff;
+  width: 600px;
+  height: fit-content;
+  float: right;
+  margin-right: 200px;
+`;
+
+export const Header = styled.div`
+  height: 70px;
+  background-color: #f4e7c5;
+`;
+
+export const TipBannerWrap = styled.div`
+  height: 100px;
+  background-color: darkgray;
+  text-align: center;
+`;
+
+export const TipBanner = styled.div`
+  color: #fff;
+  font-size: 16px;
+  padding: 30px 0 0 0;
+`;
+
+export const Line = styled.div`
+  border: #d9d9d9 4px solid;
+`;
+
+export const MenuWrap = styled.div`
+  margin: 10px 0 15px 0;
+`;
+
+export const MenuTitle = styled.div`
+  display: flex;
+  font-size: 25px;
+  font-weight: bold;
+  margin: 20px 0 10px 30px;
+  float: left;
+`;
+
+export const MenuSubTilte = styled.div`
+  font-size: 16px;
+  font-weight: normal;
+  margin: 10px 0 10px 10px;
+`;
+
+export const MenuImg = styled.img`
+  display: flex;
+  margin: 10px auto 0 auto;
+  background-color: #96a38d;
+  height: 220px;
+  width: 96%;
+  border-radius: 15px;
+`;
+
+export const Footer = styled.div`
+  text-align: center;
+  font-weight: bold;
+  font-size: 20px;
+  padding: 10px;
+  height: 70px;
+  background-color: #d9d9d9;
+`;
---
+++ src/pages/MainPage/MainPage.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import {
+  BackTitle,
+  Footer,
+  Header,
+  Line,
+  MenuImg,
+  MenuSubTilte,
+  MenuTitle,
+  MenuWrap,
+  PageWrapper,
+  TipBanner,
+  TipBannerWrap,
+} from '../../PageStyles';
+import menu from '../../assets/222.svg';
+
+function MainPage() {
+  return (
+    <div>
+      <BackTitle>
+        Find a Delicious Slice
+        <br />
+        at the Bake Shop
+      </BackTitle>
+      <PageWrapper>
+        <Header>헤더</Header>
+        <TipBannerWrap>
+          <TipBanner>팁 배너임</TipBanner>
+        </TipBannerWrap>
+        <Line />
+        <MenuWrap>
+          <MenuTitle>
+            봉사 모임 구하기
+            <MenuSubTilte>같이 봉사활동해요~</MenuSubTilte>
+          </MenuTitle>
+          <MenuImg src={menu} />
+        </MenuWrap>
+        <Line />
+        <MenuWrap>
+          <MenuTitle>
+            봉사 모임 구하기
+            <MenuSubTilte>같이 봉사활동해요~</MenuSubTilte>
+          </MenuTitle>
+          <MenuImg src={menu} />
+        </MenuWrap>
+        <Line />
+        <MenuWrap>
+          <MenuTitle>
+            봉사 모임 구하기
+            <MenuSubTilte>같이 봉사활동해요~</MenuSubTilte>
+          </MenuTitle>
+          <MenuImg src={menu} />
+        </MenuWrap>
+        <Line />
+
+        <Footer>푸터</Footer>
+      </PageWrapper>
+    </div>
+  );
+}
+
+export default MainPage;
